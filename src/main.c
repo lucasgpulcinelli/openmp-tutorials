d6c0526bfd794c73c6668f0c89cdfc39bfd9c7c3
lucasgpulcinelli <lucasegp@usp.br>
2023-12-05 22:39:50 -0300

    add exercise 5, with master and nowait (second one not needed, but just to show)

--- src/main.c
+++ src/main.c
@@ -3,19 +3,29 @@
 #include <stdlib.h>
 
 int main() {
+  int threads;
   int steps = 1000000000;
   double step = 1 / (double)steps;
 
   double time = omp_get_wtime();
 
   double sum = 0;
-#pragma omp parallel for reduction(+ : sum)
-  for (int i = 0; i < steps; i++) {
-    double x = (i + 0.5) * step;
-    sum += 4 / (1 + x * x);
+
+#pragma omp parallel
+  {
+
+#pragma omp master
+    { threads = omp_get_num_threads(); }
+
+#pragma omp for reduction(+ : sum) nowait
+    for (int i = 0; i < steps; i++) {
+      double x = (i + 0.5) * step;
+      sum += 4 / (1 + x * x);
+    }
   }
 
   sum *= step;
 
-  printf("result: %f\ntook %f seconds\n", sum, omp_get_wtime() - time);
+  printf("result: %f\ntook %f seconds\nwith %d threads\n", sum,
+         omp_get_wtime() - time, threads);
 }
