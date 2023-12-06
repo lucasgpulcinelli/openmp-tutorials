bf75d5e43ea320d4244e935019ee31f80009161a
lucasgpulcinelli <lucasegp@usp.br>
2023-12-05 22:27:53 -0300

    add exercise 4, with reduction clauses

--- src/main.c
+++ src/main.c
@@ -3,30 +3,19 @@
 #include <stdlib.h>
 
 int main() {
-  int threads;
   int steps = 1000000000;
   double step = 1 / (double)steps;
-  double result = 0;
 
   double time = omp_get_wtime();
-#pragma omp parallel
-  {
-    double sum = 0;
-    int id = omp_get_thread_num();
-    if (id == 0) {
-      threads = omp_get_num_threads();
-    }
 
-#pragma omp for schedule(static)
-    for (int i = 0; i < steps; i++) {
-      double x = (i + 0.5) * step;
-      sum += 4 / (1 + x * x);
-    }
-
-#pragma omp critical
-    result += sum * step;
+  double sum = 0;
+#pragma omp parallel for reduction(+ : sum)
+  for (int i = 0; i < steps; i++) {
+    double x = (i + 0.5) * step;
+    sum += 4 / (1 + x * x);
   }
 
-  printf("result: %f\ntook %f seconds\nwith %d threads\n", result,
-         omp_get_wtime() - time, threads);
+  sum *= step;
+
+  printf("result: %f\ntook %f seconds\n", sum, omp_get_wtime() - time);
 }
